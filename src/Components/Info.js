5e56d1729b8540feccbceb9bdabc54b57ee95fd0
smitbhimani1 <108757028+smitbhimani1@users.noreply.github.com>
2023-11-08 00:34:53 +0530

    main

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -15,17 +15,31 @@ function Info() {
           <span>Know your Dr. Praksh</span>
         </h3>
         <p className="info-description">
-          Dr. Prakash Bhatt is s a young and dynamic Neurologist in Bhavnagar.
-          <br></br>
-          He completed his MD from Nazareth Hospital, Shillong, and his DNB in
-          Neurology at Sahyadri Speciality Hospital. <br></br>
-          He manages Acute and Chronic Neurological Disorders, such as disorders
-          of the Brain, Spine, Nerves and Muscles. <br></br>
-          Dr. Prakash is open-minded in his approach and utilises both
-          traditional and non-traditional methods of care. Since beginning his
-          practice, he has strived to go above and beyond, taking the time to
-          get to the root of his patient’s problems to improve their quality of
-          life. <br></br>
+          <p>
+            &bull; Dr. Prakash Bhatt is s a young and dynamic Neurologist in
+            Bhavnagar.{" "}
+          </p>
+          <p>
+            &bull; He completed his MD from Nazareth Hospital, Shillong, and his
+            DNB in Neurology at Sahyadri Speciality Hospital.
+          </p>
+          <p>
+            &bull; He completed his MD from Nazareth Hospital, Shillong, and his
+            DNB in Neurology at Sahyadri Speciality Hospital.
+          </p>
+          <p>
+            &bull; He manages Acute and Chronic Neurological Disorders, such as
+            disorders of the Brain, Spine, Nerves and Muscles.
+          </p>
+          <p>
+            &bull; Dr. Prakash is open-minded in his approach and utilises both
+            traditional and non-traditional methods of care.
+          </p>
+          <p>
+            &bull; Since beginning his practice, he has strived to go above and
+            beyond, taking the time to get to the root of his patient’s problems
+            to improve their quality of life.
+          </p>
         </p>
       </div>
       <div className="info-title-content">
@@ -33,14 +47,25 @@ function Info() {
           <span>Know your Dr. Kirti</span>
         </h3>
         <p className="info-description">
-          Dr Kirti Jalela Bhatt is a clinical neurophysiologist. She did her EMG
-          fellowship from Bombay Hospital and Medical research centre, Mumbai,
-          under the mentorship of esteemed neurophysiologist Dr Khushnuma
-          Mansukhani. Subsequently she worked as a neurophysiologist in Bombay
-          hospital, Mumbai. We have an International standard EMG machine. In
-          our clinic, we ensure that all EMG and EP studies are done by Dr
-          Kirti, not by technicians. Advantage of special training in this field
-          is reflected in test results and proper treatment decisions.
+          <p>&bull; Dr Kirti Jalela Bhatt is a clinical neurophysiologist.</p>
+          <p>
+            &bull; She did her EMG fellowship from Bombay Hospital and Medical
+            research centre, Mumbai, under the mentorship of esteemed
+            neurophysiologist Dr Khushnuma Mansukhani.
+          </p>
+          <p>
+            &bull; Subsequently she worked as a neurophysiologist in Bombay
+            hospital, Mumbai.
+          </p>
+          <p>&bull; We have an International standard EMG machine.</p>
+          <p>
+            &bull; In our clinic, we ensure that all EMG and EP studies are done
+            by Dr Kirti, not by technicians.
+          </p>
+          <p>
+            &bull; Advantage of special training in this field is reflected in
+            test results and proper treatment decisions.
+          </p>
         </p>
       </div>
 
